0707ec75f552e431f8124d0b468206e39e2e4c58
hemanthreddy31 <hemanthkankanala1@gmail.com>
2023-07-08 23:18:35 +0530

    initial commit

---
+++ NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Data;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        private readonly NZWalksDbContext _context;
+        public RegionsController(NZWalksDbContext context)
+        {
+            this._context = context;
+        }
+
+
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var regions = _context.Regions.ToList();
+            return Ok(regions);
+        }
+
+        [HttpGet]
+        [Route("{id:Guid}")]
+        public IActionResult GetById([FromRoute] Guid id)
+        {
+            //var region = _context.Regions.Find(id);
+            var region = _context.Regions.FirstOrDefault(r => r.Id == id);
+            if (region == null)
+            {
+                return NotFound();
+            }
+            return Ok(region);
+        }
+    }
+}
+/* var regions = new List<Region>
+            {
+                new Region
+                {
+                    Id=Guid.NewGuid(),
+                    Name="Hyderabad",
+                    Code="Hyd",
+                    RegionImageUrl="https://www.holidify.com/images/bgImages/HYDERABAD.jpg"
+                },
+                  new Region
+                {
+                    Id=Guid.NewGuid(),
+                    Name="Karimnagar",
+                    Code="Knr",
+                    RegionImageUrl="https://www.holidify.com/images/bgImages/HYDERABAD.jpg"
+                }
+            };*/
