0e5cd6cd065fd4f80afcc8f7d57c12cc6e84b719
hemanthreddy31 <hemanthkankanala1@gmail.com>
2023-07-09 16:50:26 +0530

    walks create method is developed along with domain and dto

---
+++ NZWalks.API/Controllers/WalksController.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Models.Domain;
+using NZWalks.API.Models.DTO;
+using NZWalks.API.Repository;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WalksController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+        private readonly IWalkRepository _walkRepository;
+
+        public WalksController(IMapper mapper, IWalkRepository walkRepository)
+        {
+            this._mapper = mapper;
+            this._walkRepository = walkRepository;
+        }
+        //create walks
+        //POST/api/walks
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] AddWalkRequestDto addWalkRequestDto)
+        {
+            //Map dto to domain model
+            var walkDomainModel = _mapper.Map<Walk>(addWalkRequestDto);
+            await _walkRepository.CreateAsync(walkDomainModel);
+            //map domain model to dto
+
+            return Ok(_mapper.Map<WalkDto>(walkDomainModel));
+
+            
+        } 
+    }
+}
---
+++ NZWalks.API/Repository/SQLWalkRepository.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Data;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Repository
+{
+    public class SQLWalkRepository : IWalkRepository
+    {
+        private readonly NZWalksDbContext _dbContext;
+
+        public SQLWalkRepository(NZWalksDbContext dbContext)
+        {
+            this._dbContext = dbContext;
+        }
+        public async Task<Walk> CreateAsync(Walk walk)
+        {
+             await _dbContext.Walks.AddAsync(walk);
+             await _dbContext.SaveChangesAsync();
+             return walk;
+        }
+    }
+}
